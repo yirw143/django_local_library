72ee9ad5174dc0cd7611ed671f8c79f7a9b87c5d
yirw143 <yirw@dukes.jmu.edu>
2025-02-16 07:26:29 -0500

    Completed part 3, 4, 5

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Language)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+admin.site.register(Author, AuthorAdmin)
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+admin.site.register(Book, BookAdmin)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+admin.site.register(BookInstance, BookInstanceAdmin)
