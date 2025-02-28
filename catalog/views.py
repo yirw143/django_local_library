22e3ea41b9ccc312597b0b8290d87b75483f5916
yirw143 <yirw@dukes.jmu.edu>
2025-02-28 05:31:39 -0500

    Completed part 7

--- catalog/views.py
+++ catalog/views.py
@@ -16,12 +16,18 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
