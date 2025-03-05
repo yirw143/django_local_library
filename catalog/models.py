6f4c90fd3f417b88118050e5efa52c09b88c290b
yirw143 <yirw@dukes.jmu.edu>
2025-03-05 18:45:46 -0500

    Completed steps 8 and 9

--- catalog/models.py
+++ catalog/models.py
@@ -49,6 +49,8 @@ class Book(models.Model):
     genre = models.ManyToManyField(
         Genre, help_text="Select a genre for this book")
 
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
